e434b8122d9d5cc4916e47975cc04964b9e76d22
mozilovich <mozilqaa1@gmail.com>
2017-04-12 21:32:12 +0400

    update 3

--- BinarySearch/Tree.java
+++ BinarySearch/Tree.java
@@ -54,32 +54,65 @@ public class Tree<T extends Comparable<T>> {
   }
 
   private void removing(T value, Node<T> node) {
-    Node<T> curr = null;
-    if (contains(value)) {
-      while (value.compareTo(node.value) != 0) {
-        if (value.compareTo(node.value) > 0 && node.right != null) {
-          curr = node.right;
-          find(value, node.right);
-        } else {
-          curr = node.left;
-          find(value, node.left);
+    Node<T> curr = find(value, node);
+
+    if (curr.value.compareTo(value)==0) {
+      if (curr.left == null) {
+        if(curr.value.compareTo(parent.value)<0) {
+          curr = parent.left.right;
+          parent.left = curr;
         }
-      }
-      if (curr.left == null && curr.right != null) {
-        curr = curr.right;
-      } else if (curr.left != null && curr.right == null) {
-        curr = curr.left;
+        else{
+          curr = parent.right.right;
+          parent.right = curr;
+        }
+
+      } else if (curr.right == null) {
+        if(curr.value.compareTo(parent.value)<0) {
+          curr = parent.left.left;
+          parent.left = curr;
+        }
+        else {
+          curr = parent.right.left;
+          parent.right = curr;
+        }
+
       } else {
         if (curr.value.compareTo(parent.value) >= 0) {
-          curr = curr.right;
-          curr.right = null;
+          curr = parent.right.right;
+          Node<T> result = curr;
+          Node<T> parent2 = parent;
+          while(result.left!=null) {
+            parent2 = result;
+            result = result.left;
+          }
+          parent2.left = null;
+          result.right = parent.right.right;
+          result.left = parent.right.left;
+          parent.right = result;
+
         } else {
-          curr = curr.left;
-          curr.left = null;
+          curr = parent.left.left;
+          Node<T> result = curr;
+          Node<T> parent2 = parent;
+          while(result.right!=null) {
+            parent2 = result;
+            result = result.right;
+          }
+          parent2.right = null;
+          result.left = parent.left.left;
+          result.right = parent.left.right;
+          parent.left = result;
+
         }
       }
     }
   }
-
+public void zig(Node<T> y ,Node<T>  x ){
+  y.right=x;
+  root = x;
+  x.left = y;
+  y.right = y.right.left;
+}
 
 }
